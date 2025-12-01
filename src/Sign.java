ad6610bd55cb672b2e388046620f03265de89338
HunterYarmy <huntery5@nycstudents.net>
2025-12-01 15:34:32 -0500

    idk bro

--- src/Sign.java
+++ src/Sign.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Sign {
+    private String message;
+    private int width;
+
+    public Sign(String message, int width) {
+        this.message = message;
+        this.width = width;
+    }
+
+    public int numberOfLines() {
+        String iMessage = message;
+        int lines = 0;
+
+        while (iMessage.length() != 0) {
+            iMessage = iMessage.substring(0, width);
+        }
+    }
+
+    public String getLines() {
+
+    }
+
 }
